2ce2a7f7bed2095ca72b6643bbd73e6ec4ebea88
Slobodan Komazec <skomazec@xorantech.com>
2023-03-11 20:12:16 +0100

    added header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,28 @@
 import "./header.css";
+import CTA from "./CTA";
+import ME from "../../assets/me.png";
+import HeaderSocials from "./HeaderSocials";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Slobodan Komazec</h1>
+        <h5 className="text-light">Developer</h5>
+        <CTA />
+        <HeaderSocials />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
-import ReactDom from "react-dom";
+// import ReactDom from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDom.render(<App />, document.querySelector("#root"));
+// ReactDom.render(<App />, document.querySelector("#root"));
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+// 👇️ if you use TypeScript, add non-null (!) assertion operator
+// const root = createRoot(rootElement!);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
