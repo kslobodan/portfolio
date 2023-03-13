f82d4fbbd93e7800e3415346e83fbb7fa4619656
Slobodan Komazec <skomazec@xorantech.com>
2023-03-14 00:56:04 +0100

    added footer and contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,69 @@
 import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { RiSkypeLine } from "react-icons/ri";
+import { BsLinkedin } from "react-icons/bs";
 
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>k.slobodan@yahoo.com</h5>
+            <a href="mailto: slobodan@yahoo.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiSkypeLine className="contact__option-icon" />
+            <h4>Skype</h4>
+            <h5>bobann985</h5>
+            <a
+              href="https://join.skype.com/invite/HNwlR2Obg377"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsLinkedin className="contact__option-icon" />
+            <h4>LinkedIn</h4>
+            <h5>Profile</h5>
+            <a
+              href="https://www.linkedin.com/in/slobodan-komazec/"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/*END OF CONTACT OPTIONS */}
+        <form action="">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,41 @@
 import "./footer.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        EGATOR
+      </a>
+
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__copyright">
+        <small>&copy; Egator Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
